90241a17b7b47a8d86dab3392651dee413d37958
丁晓东 <dingxiaodongaaa@163.com>
2020-07-14 09:54:47 +0800

    自定义 hash 模式路由完成

--- src/hashrouter/index.js
+++ src/hashrouter/index.js
@@ -1,4 +1,4 @@
-const _Vue = null
+let _Vue = null
 
 export default class HashRouter {
   static install (Vue) {
@@ -16,7 +16,7 @@ export default class HashRouter {
       beforeCreate () {
         // 添加判断，避免创建组件的时候注入 router
         if (this.$options.router) {
-          _Vue.prototype.$router = this.$options.router,
+          _Vue.prototype.$router = this.$options.router
           this.$options.router.init()
         }
       }
@@ -26,5 +26,67 @@ export default class HashRouter {
   constructor (options) {
     this.$options = options
     this.routeMap = {}
+    // 使用 Vue 提供的 observable 将路由地址转换成响应式数据
+    this.data = _Vue.observable({
+      // 记录当前路由地址的属性
+      // 如果当前地址有 hash 值，就初始化当前地址的 hash 如果没有默认为 /
+      // 解决页面 F5 刷新之后页面组件和路由不对应的问题
+      current: window.location.hash.slice(1) || '/'
+    })
+  }
+
+  init () {
+    this.createRouteMap()
+    this.initComponents(_Vue)
+    this.initEvent()
+  }
+
+  createRouteMap () {
+    // 遍历所有的路由规则并解析成键值对的形式存储到 routeMap 这个对象里
+    this.$options.routes.forEach(route => {
+      this.routeMap[route.path] = route.component
+    })
+  }
+
+  initComponents (Vue) {
+    const self = this
+    Vue.component('router-link', {
+      props: {
+        to: String
+      },
+      // render 函数接收 h 函数作为参数，h 函数用于创建虚拟 DOM ，这个 h 函数是 Vue 传过来的
+      render (h) {
+        return h('a', {
+          attrs: {
+            href: this.to
+          },
+          on: {
+            click: this.clickHandler
+          }
+        }, [this.$slots.default])
+      },
+      methods: {
+        clickHandler (e) {
+          // 跳转路由
+          window.location.hash = this.to
+          // 更新 current
+          this.$router.data.current = this.to
+          // 组织 a 标签默认跳转
+          e.preventDefault()
+        }
+      }
+    })
+    Vue.component('router-view', {
+      render (h) {
+        const component = self.routeMap[self.data.current]
+        return h(component)
+      }
+    })
+  }
+
+  initEvent () {
+    window.addEventListener('hashchange', () => {
+      this.data.current = window.location.hash.slice(1)
+    })
   }
 }
